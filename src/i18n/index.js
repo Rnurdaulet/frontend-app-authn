ed08e9eb5e358ee916b793f8249ac62eef950014
Rnurdaulet <rakhymberdin@gmail.com>
2025-08-20 18:21:09 +0500

    i18

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,1 +1,69 @@
-export default [];
+import af_ZA from './messages/af_ZA.json';
+import ar from './messages/ar.json';
+import az from './messages/az.json';
+import da from './messages/da.json';
+import de_DE from './messages/de_DE.json';
+import de from './messages/de.json';
+import el from './messages/el.json';
+import es_419 from './messages/es_419.json';
+import es_ES from './messages/es_ES.json';
+import fa from './messages/fa.json';
+import fr_CA from './messages/fr_CA.json';
+import he from './messages/he.json';
+import hi from './messages/hi.json';
+import hu from './messages/hu.json';
+import id from './messages/id.json';
+import it_IT from './messages/it_IT.json';
+import ja from './messages/ja.json';
+import kk_KZ from './messages/kk-kz.json';
+import lv from './messages/lv.json';
+import pt_BR from './messages/pt_BR.json';
+import pt_PT from './messages/pt_PT.json';
+import ro from './messages/ro.json';
+import ru from './messages/ru.json';
+import sv from './messages/sv.json';
+import sw from './messages/sw.json';
+import te from './messages/te.json';
+import th from './messages/th.json';
+import tr_TR from './messages/tr_TR.json';
+import uk from './messages/uk.json';
+import uz from './messages/uz.json';
+import vi from './messages/vi.json';
+import zh_CN from './messages/zh_CN.json';
+import zh_HK from './messages/zh_HK.json';
+
+export default [
+  { locale: 'af_ZA', messages: af_ZA },
+  { locale: 'ar', messages: ar },
+  { locale: 'az', messages: az },
+  { locale: 'da', messages: da },
+  { locale: 'de_DE', messages: de_DE },
+  { locale: 'de', messages: de },
+  { locale: 'el', messages: el },
+  { locale: 'es_419', messages: es_419 },
+  { locale: 'es_ES', messages: es_ES },
+  { locale: 'fa', messages: fa },
+  { locale: 'fr_CA', messages: fr_CA },
+  { locale: 'he', messages: he },
+  { locale: 'hi', messages: hi },
+  { locale: 'hu', messages: hu },
+  { locale: 'id', messages: id },
+  { locale: 'it_IT', messages: it_IT },
+  { locale: 'ja', messages: ja },
+  { locale: 'kk-KZ', messages: kk_KZ },
+  { locale: 'lv', messages: lv },
+  { locale: 'pt_BR', messages: pt_BR },
+  { locale: 'pt_PT', messages: pt_PT },
+  { locale: 'ro', messages: ro },
+  { locale: 'ru', messages: ru },
+  { locale: 'sv', messages: sv },
+  { locale: 'sw', messages: sw },
+  { locale: 'te', messages: te },
+  { locale: 'th', messages: th },
+  { locale: 'tr_TR', messages: tr_TR },
+  { locale: 'uk', messages: uk },
+  { locale: 'uz', messages: uz },
+  { locale: 'vi', messages: vi },
+  { locale: 'zh_CN', messages: zh_CN },
+  { locale: 'zh_HK', messages: zh_HK },
+];
